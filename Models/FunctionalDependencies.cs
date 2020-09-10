49fd25e24f3c3209bf525019e881309ae9fd34c4
Administrator <Administrator@WIN-41289KKFONO>
2020-09-10 14:33:59 -0700

    Project files.

---
+++ Models/FunctionalDependencies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NormalizationPWA.Models
+{
+    public class FunctionalDependencies
+    {
+        public string Lhs { get; set; }
+        public string Rhs { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ Models/NormalFormTables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NormalizationPWA.Models
+{
+    public class NormalFormTables
+    {
+        public string TableName { get; set; }
+        public string FDs { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
